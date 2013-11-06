5294742c6368baae7500fbad0e23b19aad3b4451
Pascal Attwenger <pascal.attwenger@gmail.com>
2013-11-06 20:52:35 +0100

    Test class

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author Pascal Attwenger (1200595)
+ * Testing if git and eclipse work together how they should
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("I love lamp");
+
+	}
+
+}
