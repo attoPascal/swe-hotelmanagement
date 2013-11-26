7fb36d50bf74f219ab6818eafb3948dcfa53a113
Vincent Reiner <vink1337@gmail.com>
2013-11-26 13:17:43 +0100

    added missing classes

---
+++ src/hm/AbstractUser.java
@@ -0,0 +1,51 @@
+package hm;
+
+public abstract class AbstractUser {
+
+	/**
+	 * Benutzername des Benutzers
+	 * @uml.property  name="username"
+	 */
+	protected String username;
+	
+	/**
+	 * Passwort des Benutzers
+	 * @uml.property  name="password"
+	 */
+	protected String password;
+
+	/**
+	 * @return
+	 * @uml.property  name="username"
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 * @uml.property  name="username"
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return
+	 * @uml.property  name="password"
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 * @uml.property  name="password"
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
