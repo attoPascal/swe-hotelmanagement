8934a495fa51ab33626244592dedd64eecf8e0a3
Vincent Reiner <vink1337@gmail.com>
2013-12-10 18:01:27 +0100

    updated zimmerbuchenjsp and BuchungsManagement.java

--- src/hm/servlets/BuchungsManagement.java
+++ src/hm/servlets/BuchungsManagement.java
@@ -5,6 +5,8 @@ import hm.Buchung;
 import hm.Hotel;
 import hm.Kategorie;
 import hm.Zimmer;
+import hm.dao.DAO;
+import hm.dao.SerializedDAO;
 
 import java.io.IOException;
 
@@ -14,6 +16,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
 
 public class BuchungsManagement extends HttpServlet {
 	/**
@@ -24,23 +29,37 @@ public class BuchungsManagement extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// reading the user input
-		String name = request.getParameter("name");
+		
+		Enumeration<String> e = request.getParameterNames();
+		
+		
+		while (e.hasMoreElements()){
+		System.out.println(e.nextElement());}
+		
+		String name = request.getParameter("select");
+		
+		int day = Integer.parseInt(request.getParameter("day"));
+		int month = Integer.parseInt(request.getParameter("months"));
+		int year = Integer.parseInt(request.getParameter("year"));
+
+		System.out.println(day +' '+ month +' '+ year);
+		
+		DAO dao = new SerializedDAO("data.ser");
+    	
+    	Hotel hotel = dao.getHotelByName("CrazySharkyFish");
+    	
+    	neueBuchung(hotel.getKategorie(name), new Aufenthalt(new Date(), 1));
+		
+		
 		PrintWriter out = response.getWriter();
-		out.println(createHotel(name));
+		out.println(createHotel("name"));
 	}
 
 	String createHotel(String name) {
 
-		Hotel hotel = new Hotel(name);
-
-		hotel.addZimmer(new Zimmer(5));
-
-		hotel.addKategorie(new Kategorie("Suite", 50, "Fernseher, Waschbecken"));
-		
-		hotel.addKategorie(new Kategorie("Absteige", 50, "Eimer zum reinkacken"));
-
-
-		hotel.getKategorie("Suite").addZimmer(hotel.getZimmer(5));
+		DAO dao = new SerializedDAO("data.ser");
+    	
+    	Hotel hotel = dao.getHotelByName("CrazySharkyFish");
 
 		return hotel.toString();
 
