d951c6d2280aa5e6af3acab005e24a912f99a1d2
Vincent Reiner <vink1337@gmail.com>
2013-12-11 00:56:59 +0100

    removed unnecessary comments

--- src/hm/users/AbstractUser.java
+++ src/hm/users/AbstractUser.java
@@ -4,19 +4,16 @@ public abstract class AbstractUser {
 
 	/**
 	 * Benutzername des Benutzers
-	 * @uml.property  name="username"
 	 */
 	protected String username;
 	
 	/**
 	 * Passwort des Benutzers
-	 * @uml.property  name="password"
 	 */
 	protected String password;
 
 	/**
 	 * @return Name (username) des Benutzers
-	 * @uml.property  name="username"
 	 */
 	public String getUsername() {
 		return username;
@@ -24,7 +21,6 @@ public abstract class AbstractUser {
 
 	/**
 	 * @param username
-	 * @uml.property  name="username"
 	 */
 	public void setUsername(String username) {
 		this.username = username;
@@ -32,7 +28,6 @@ public abstract class AbstractUser {
 
 	/**
 	 * @return Password des Users
-	 * @uml.property  name="password"
 	 */
 	public String getPassword() {
 		return password;
@@ -40,7 +35,6 @@ public abstract class AbstractUser {
 
 	/**
 	 * @param password
-	 * @uml.property  name="password"
 	 */
 	public void setPassword(String password) {
 		this.password = password;
