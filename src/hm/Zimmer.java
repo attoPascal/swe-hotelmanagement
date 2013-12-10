fc6f52fd14af5222fd3a1d6c4eb49aae764fd086
Gerald <3111993@gmx.at>
2013-12-10 23:37:41 +0100

    Zimmer Doc

--- src/hm/Zimmer.java
+++ src/hm/Zimmer.java
@@ -20,40 +20,63 @@ public class Zimmer implements Serializable {
 	 */
 	ArrayList<Buchung> buchungen = new ArrayList<Buchung>();
 	
-	
+	/**
+	 * @param nummer initialisiert eine Zimmernummer
+	 */
 	public Zimmer(int nummer){
 		
 		this.nummer = nummer;
 		
 	}
 	
-
+	/**
+	 * @return gibt eine ArrayList an bestehenden Buchungen zurueck
+	 */
 	public ArrayList<Buchung> getBuchungen() {
 		return buchungen;
 	}
 
-
+	/**
+	 * @param initialisiert die buchungen
+	 */
 	public void setBuchungen(ArrayList<Buchung> buchungen) {
 		this.buchungen = buchungen;
 	}
 
 
-	
+	/**
+	 * @return gibt Nummer des gefragen Zimmers zurueck
+	 */
 	public int getNummer() {
 		return nummer;
 	}
 
-
+	/**
+	 * @param nummer Legt Nummer eines Zimmers fest
+	 */
 	public void setNummer(int nummer) {
 		this.nummer = nummer;
 	}
 	
+	/**
+	 * Erstellt ein Buchung-Objekt
+	 * 
+	 * @param kategorie Zimmer-Kategorie der Buchung
+	 * @param aufenthalt Zeitraum der Pruefung
+	 */
 	public void addBuchung(Kategorie kategorie, Aufenthalt aufenthalt){
 		
 		buchungen.add(new Buchung(kategorie, aufenthalt));
 
 	}
 	
+	/**
+	 * Ueberprueft, ob ein Zimmer zu einem Zeitraum(Aufenthalt) gebucht ist
+	 * 
+	 * @param zimmer Zimmer-Objekt, das ueberprueft wird
+	 * @param aufenthalt Zeitraum der gewuenschten Buchung
+	 * @return gibt true zurueck, wenn es eine ueberlappung gibt (d.h. das Zimmer bereits gebucht ist)
+	 */
 	public static boolean isBooked(Zimmer zimmer, Aufenthalt aufenthalt) {
 
 		for (Buchung buchung : zimmer.getBuchungen()) {
