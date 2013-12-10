7352d51ba10bf2040069ad381bc583bbd1c8149e
Vincent Reiner <vink1337@gmail.com>
2013-12-10 09:19:16 +0100

    added BuchungsManagement servlet

---
+++ src/hm/servlets/BuchungsManagement.java
@@ -0,0 +1,12 @@
+package hm.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class BuchungsManagement extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5095727992461101776L;
+
+}
