8f13ad59020865dcce68dbd3fae3d3667c102b8a
Vincent Reiner <vink1337@gmail.com>
2013-12-11 09:04:51 +0100

    updated package-infos

--- src/hm/dao/package-info.java
+++ src/hm/dao/package-info.java
@@ -2,7 +2,7 @@
  * 
  */
 /**
- * 
+ * Stellt Klassen zur Verfügung, die notwendig sind um Daten persisten zu speicher.
  *
  */
 package hm.dao;
--- src/hm/users/package-info.java
+++ src/hm/users/package-info.java
@@ -2,7 +2,7 @@
  * 
  */
 /**
- * 
+ * Stellt Klassen zur Verfügung, die die Benutzer des Systems repräsentieren.
  *
  */
 package hm.users;
