904fa0db12e80001326eecad0ed1bdea776961b4
Vincent Reiner <vink1337@gmail.com>
2013-12-16 11:41:19 +0100

    added some Methods necessary for the Analysts use cases

--- src/hm/managers/AnalyseManagement.java
+++ src/hm/managers/AnalyseManagement.java
@@ -3,8 +3,19 @@
  */
 package hm.managers;
 
+import java.util.ArrayList;
+
+import hm.Aufenthalt;
+import hm.Buchung;
+import hm.Hotel;
+import hm.Zimmer;
+
 /**
  * @author 
+ *
+ */
+/**
+ * @author Elisabeth Reiner
  *
  */
 public class AnalyseManagement {
@@ -16,4 +27,70 @@ public class AnalyseManagement {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	/**
+	 * Gibt einem eine Liste mit gebuchten Zimmern in einer gewissen Zeitspanne zurück
+	 * 
+	 * @param hotel
+	 * @param aufenthalt
+	 * @return
+	 */
+	public ArrayList<Zimmer> getBookedRooms(Hotel hotel, Aufenthalt aufenthalt){
+		
+		ArrayList<Zimmer> zlist = hotel.getZimmerList();
+		
+		ArrayList<Zimmer> bzlist = new ArrayList<Zimmer>();
+		
+		for (Zimmer zimmer : zlist){
+			
+			if (Zimmer.isBooked(zimmer, aufenthalt)) bzlist.add(zimmer);
+			
+		}
+		
+		return bzlist;
+	}
+	
+	/**
+	 * Gibt einem eine Liste mit freien Zimmern zu einer gewissen Zeitdauer zurück
+	 * 
+	 * @param hotel
+	 * @param aufenthalt
+	 * @return
+	 */
+	public ArrayList<Zimmer> getFreeRooms(Hotel hotel, Aufenthalt aufenthalt){
+		
+		ArrayList<Zimmer> zlist = hotel.getZimmerList();
+		
+		ArrayList<Zimmer> fzlist = new ArrayList<Zimmer>();
+		
+		for (Zimmer zimmer : zlist){
+			
+			if (!Zimmer.isBooked(zimmer, aufenthalt)) fzlist.add(zimmer);
+			
+		}
+		
+		return fzlist;
+	}
+	
+	public ArrayList<Buchung> getBookings(Hotel hotel, Aufenthalt aufenthalt){
+
+		ArrayList<Zimmer> zlist = getBookedRooms(hotel, aufenthalt);
+		ArrayList<Buchung> buchungen = new ArrayList<Buchung>();
+		for (Zimmer zimmer : zlist){
+			
+			buchungen.addAll(zimmer.getBuchungen());
+			
+		}
+		
+		return buchungen;
+	}
+	
+	public int getNumberOfBookings(Hotel hotel, Aufenthalt aufenthalt){
+		int buchungen = 0;
+	
+		buchungen = getBookings(hotel, aufenthalt).size();
+		
+		return buchungen;
+	}
+	
 }
