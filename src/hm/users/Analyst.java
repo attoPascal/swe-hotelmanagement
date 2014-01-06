195dba12f73b5bc85d6b858993caefbb048f1779
Vincent Reiner <vink1337@gmail.com>
2014-01-06 20:35:29 +0100

    fixed Analyst not extending AbstractUser

--- src/hm/users/Analyst.java
+++ src/hm/users/Analyst.java
@@ -7,13 +7,19 @@ package hm.users;
  * @author 
  *
  */
-public class Analyst {
+public class Analyst extends AbstractUser{
 
 	/**
 	 * 
 	 */
-	public Analyst() {
+	private static final long serialVersionUID = -3126007390053181157L;
+
+	/**
+	 * 
+	 */
+	public Analyst(String username, String password) {
 		// TODO Auto-generated constructor stub
+		super(username, password);
 	}
 
 }
