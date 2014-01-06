3bbe4bcbcb6cfb69c5140455fb8dfe8da2eac1a6
attoPascal <pascal@Penelope.local>
2014-01-06 18:50:08 +0100

    Methoden-Stubs für Services

--- src/hm/users/HotelGast.java
+++ src/hm/users/HotelGast.java
@@ -1,6 +1,10 @@
 package hm.users;
 
+import java.util.Date;
+import java.util.HashMap;
+
 import hm.Buchung;
+import hm.Service;
 
 /**
  * Benutzer Hotelgast, kann Buchungen vornehmen
@@ -13,11 +17,13 @@ public class HotelGast extends AbstractUser{
 	
 	private Buchung buchung;
 	private String zahlungsdaten;
+	private HashMap<Date,Service> services;
 	
 	public HotelGast(String username, String password, Buchung buchung, String zahlungsdaten) {
 		super(username, password);
 		this.buchung = buchung;
 		this.zahlungsdaten = zahlungsdaten;
+		this.services = new HashMap<Date,Service>();
 	}
 	
 	/**
@@ -48,4 +54,17 @@ public class HotelGast extends AbstractUser{
 		this.zahlungsdaten = zahlungsdaten;
 	}
 	
+	/**
+	 * Fügt ein neues Service zum angegebenen Zeitpunkt hinzu
+	 */
+	public void addService(Date date, Service service) {
+		services.put(date, service);
+	}
+	
+	/**
+	 * Gibt die HashMap aller gebuchten Services mit ihrem jeweiligen Datum zurück
+	 */
+	public HashMap<Date,Service> getServices() {
+		return services;
+	}
 }
