d9c4a2e48f20cf35d150f5229916468c791ddfa3
philipp <hiermannp@gmail.com>
2014-01-17 06:47:26 +0100

    Buchungen verwalten bearbeitet

---
+++ WebContent/buchungenverwalten.jsp
@@ -0,0 +1,133 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="hm.Hotel" %>
+<%@ page import="hm.Zimmer" %>
+<%@ page import="hm.Kategorie" %>
+<%@ page import="hm.users.Hotelier" %>
+<%@ page import="hm.servlets.BuchungsVerwaltungsServlet" %>
+<%@ page import="hm.dao.DAO" %>
+
+<%
+	//Rechte überprüfen:
+	//Hotelier mit canManageRooms
+	Object user = session.getAttribute("user");
+	if (!(user instanceof Hotelier) || !((Hotelier) user).isCanManageRooms()) {
+		session.setAttribute("alert", "Zugriff verweigert. Bitte melden Sie sich als Hotelier mit den nötigen Rechten an, um auf diese Seite zuzugreifen.");
+		session.setAttribute("redirect", "zimmerverwalten.jsp");
+		response.sendRedirect("login.jsp");
+	}
+
+	BuchungsManagement bm = new BuchungsManagement();
+	bm.getManagement().instantiateDAO();
+	ArrayList<Hotel> hList = bm.getManagement().getDAO().getHotelList();
+	
+	String hotelName = request.getParameter("hotel");
+	Hotel hotel = null;
+	
+	if (hotelName != null) {
+		hotel = zm.getManagement().getDAO().getHotelByName(hotelName);
+		
+	} else {
+		//wenn kein Hotel spezifiziert: erstes aus Liste wählen
+		hotel = hList.get(0);
+	}
+	
+	ArrayList<Zimmer> zList = hotel.getZimmerList();
+	ArrayList<Kategorie> kList = hotel.getKategorien();
+%>
+
+<!DOCTYPE html>
+<html>
+
+<%@ include file="inc/head.jsp" %>
+
+<body>
+	<%@ include file="inc/nav.jsp" %>
+	
+	<main class="container">
+		<h1>Zimmer verwalten</h1>
+		
+		<form>
+			<div class="form-group">
+    			<label for="setHotel">Hotel auswählen:</label>
+    			<select name="hotel" class="set-hotel form-control" id="setHotel">
+    				<% for (Hotel h : hList) { 
+					String selected = (h.getName().equals(hotel.getName())) ? "selected=\"selected\"" : "";
+					%>
+					<option value="<%= h.getName() %>"<%= selected %>><%= h.getName() %></option>
+					<% } %>
+				</select>
+  			</div>
+		</form>
+		
+		<p id="response" class="alert alert-success"></p>
+		
+		<div id="manage">
+			<table class="zimmer table">
+				<tr>
+					<th class="zimmer">Zimmer</th>
+					<th class="kategorie">Kategorie</th>
+					<th class="button"></th>
+				</tr>
+		
+				<% for (Zimmer z : zList) { %>
+				<tr>
+					<td class="zimmer">
+						<input type="text" class="form-control" value="<%= z.getNummer() %>" readonly>
+					</td>
+					<td class="kategorie">
+						<select class="set-kategorie form-control" data-zimmer="<%= z.getNummer() %>">
+							<% for (Kategorie k : kList) {
+								String selected = (k.hasZimmer(z.getNummer())) ? " selected" : "";
+							%>
+							<option value="<%= k.getName() %>"<%= selected %>><%= k.getName() %></option>
+							<% } %>
+						</select>
+					</td>
+					<td class="button">
+						<form action="ZimmerServlet" method="get">
+							<input type="hidden" name="action" value="delete">
+							<input type="hidden" name="hotel" value="<%= hotel.getName() %>">
+							<input type="hidden" name="zimmer" value="<%= z.getNummer() %>">
+							
+							<button type="submit" class="btn btn-danger">
+								<span class="glyphicon glyphicon-remove"></span>
+							</button>
+						</form>
+					</td>
+				</tr>
+				<% } %>
+			</table>
+			
+			<form action="ZimmerServlet" method="get">
+				<table class="zimmer table">
+					<tr>
+						<td class="zimmer">
+							<input type="hidden" name="action" value="create">
+							<input type="hidden" name="hotel" value="<%= hotel.getName() %>">
+							<input type="text" name="zimmer" class="form-control">
+						</td>
+						<td class="kategorie">
+							<select name="kategorie" class="form-control">
+								<option value="" selected disabled>Bitte auswählen:</option>
+								<% for (Kategorie k : kList) { %>
+								<option value="<%= k.getName() %>"><%= k.getName() %></option>
+								<% } %>
+							</select>
+						</td>
+						<td class="button">
+							<button type="submit" class="btn btn-success">
+								<span class="glyphicon glyphicon-plus"></span>
+							</button>
+							<button type="submit" class="aufenthalt">
+								<span class="glyphicon glyphicon-plus"></span>
+							</button>
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</main>
+</body>
+
+</html>
--- src/hm/managers/BuchungsManagement.java
+++ src/hm/managers/BuchungsManagement.java
@@ -7,6 +7,7 @@ import hm.Aufenthalt;
 import hm.Kategorie;
 import hm.Zimmer;
 import hm.Buchung;
+import hm.Hotel;
 import hm.dao.*;
 
 public class BuchungsManagement {
@@ -33,52 +34,113 @@ public class BuchungsManagement {
 	 *            Kategorie des Zimmers
 	 * @param aufenthalt
 	 *            Zeitraum, der gebucht wurde
-	 *            
-	 * @param name Name das Hotels für das eine neue Buchung erstellt werden soll
+	 * 
+	 * @param name
+	 *            Name das Hotels für das eine neue Buchung erstellt werden soll
 	 * @return gibt die Nummer des Zimmers zurueck, das gebucht wurde.
 	 */
-	public int createBuchung(Kategorie kategorie, Aufenthalt aufenthalt, String name) {
+	public int createBuchung(Kategorie kategorie, Aufenthalt aufenthalt,
+			String name) {
 		Zimmer zimmer = kategorie.getZimmer(aufenthalt);
 		zimmer.addBuchung(kategorie, aufenthalt);
 
 		return zimmer.getNummer();
 	}
-	
-	
+
 	/**
 	 * @param buchung
 	 * @param aufenthalt
-	 * @param name Name des Hotes für das die Zeit einer Buchung verändert werden soll
+	 * @param name
+	 *            Name des Hotes für das die Zeit einer Buchung verändert werden
+	 *            soll
 	 * @return
 	 */
-	public String editAufenthalt(Buchung buchung, Aufenthalt aufenthalt, String name) {
-		return null;
+	public String editAufenthalt(Buchung buchung, Aufenthalt aufenthalt,
+			String name) throws ClassNotFoundException, FileNotFoundException,
+			IOException {
+
+		Hotel hotel = dao.getHotelByName(name);
+		Zimmer zimmer = hotel.getZimmer(buchung.getZimmernummer());
+		
+		/**
+		 * Sollte das Zimmer im Hotel nicht vorhanden sein
+		 */
+		if (zimmer == null) 
+			return "Das Zimmer existiert nicht";
+
+		if (Zimmer.isBooked(zimmer, aufenthalt)) {
+			return "Das Zimmer ist du diesem Zeitpunkt leider schon belegt";
+
+		} else {
+			buchung.setAufenthalt(aufenthalt);
+			return "Der Aufenthalt wurde erfolgreich editiert";
+		}
 	}
-	
+
 	/**
 	 * @param buchung
 	 * @param zimmer
-	 * @param name Name des Hotels für das die Zimmernummer einer Buchung geändert werden soll
+	 * @param name
+	 *            Name des Hotels für das die Zimmernummer einer Buchung
+	 *            geändert werden soll
 	 */
-	public void editZimmerNummer(Buchung buchung, Zimmer zimmer, String name) {
+	/* TODO Es wäre vermutlich sinnvoller, anstatt eines Zimmerobjekts nur die Nummer zu übergeben*/
+	public String editZimmerNummer(Buchung buchung, Zimmer zimmer, String name)
+			throws ClassNotFoundException, FileNotFoundException, IOException {
 		
+		Hotel hotel = dao.getHotelByName(name);
+		/**
+		 * Das übergebene Zimmerobjekt könnte zB nicht zu dem ausgewählten Hotel
+		 * gehören, deshalb ist es notwendig, dies zu überprüfen
+		 */
+		Zimmer room = hotel.getZimmer(zimmer.getNummer());
+		
+		/**
+		 * Sollte das Zimmer im Hotel nicht vorhanden sein
+		 */
+		if (room == null) 
+			return "Das Zimmer existiert nicht";
+		
+		if (Zimmer.isBooked(room, buchung.getAufenthalt())) {
+			return "Das Zimmer ist zu diesem Zeitpunkt schon belegt";
+		
+		} else {
+			/**
+			 * Das alte Zimmer aus der Buchung entfernen
+			 */
+			buchung.getZimmer().removeBuchung(buchung.getId());
+			/**
+			 * Dem neuen Zimmer eine Buchung hinzufügen
+			 */
+			room.addBuchung(buchung.getKategorie(), buchung.getAufenthalt());
+			/**
+			 * Das Zimmer der Buchung zuteilen
+			 */
+			buchung.setZimmer(room);
+
+			return "Das Zimmer für die Buchung wurde erfolgreich geändert";
+		}
 	}
 
-	
 	/**
 	 * Storniert eine bestehenede Buchung/ Buchung wird entfernt
-	 * @param id ID der Buchung
-	 * @param nummer Nummer des Zimmers von der die Buchung entfernt werden soll
-	 * @param name Name des Hotels von dem die Buchung entfernt werden soll
+	 * 
+	 * @param id
+	 *            ID der Buchung
+	 * @param nummer
+	 *            Nummer des Zimmers von der die Buchung entfernt werden soll
+	 * @param name
+	 *            Name des Hotels von dem die Buchung entfernt werden soll
 	 * @throws FileNotFoundException
 	 * @throws ClassNotFoundException
 	 * @throws IOException
 	 */
-	public void removeBuchung(int id, int nummer, String name) throws FileNotFoundException, ClassNotFoundException, IOException {
+	public void removeBuchung(int id, int nummer, String name)
+			throws FileNotFoundException, ClassNotFoundException, IOException {
 		Zimmer zimmer = dao.getHotelByName(name).getZimmer(nummer);
 		zimmer.removeBuchung(id);
 	}
-	
+
 	public DAO getDAO() {
 		return dao;
 	}
