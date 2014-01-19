44c0713423088120a2ef6038cde9969f602fa3d5
Pascal Attwenger <pascal.attwenger@gmail.com>
2014-01-19 15:24:51 +0100

    Klasse ServiceException

---
+++ src/hm/exceptions/ServiceException.java
@@ -0,0 +1,17 @@
+package hm.exceptions;
+
+/**
+ * Exception-Handling im ServiceManagement
+ */
+public class ServiceException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ServiceException(String message) {
+		super(message);
+	}
+	
+	public ServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
